8e44b4174a57c0643dc173cfb9c5c098ef399bd5
Fito <jose.gonzalez@arkusnexus.com>
2022-05-16 14:31:08 -0700

    added bounding box

--- app/src/main/java/com/test/cvrewarm/MainActivity.java
+++ app/src/main/java/com/test/cvrewarm/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.WindowManager;
 import android.widget.Toast;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+
+import org.opencv.core.MatOfInt;
 import org.opencv.dnn.Dnn;
 import org.opencv.dnn.Net;
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,6 +38,23 @@ import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.util.Random;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraActivity;
+import java.util.Collections;
+
+
+
+
 public class MainActivity extends AppCompatActivity  implements /*OnTouchListener,*/ CvCameraViewListener2 {
     private static final String  TAG              = "MainActivity";
 
@@ -48,6 +67,8 @@ public class MainActivity extends AppCompatActivity  implements /*OnTouchListene
     private Size                 SPECTRUM_SIZE;
     private Scalar               CONTOUR_COLOR;
     private Net net;
+    private static List<String> classNames;
+    private static List<Scalar> colors=new ArrayList<>();
     private CameraBridgeViewBase mOpenCvCameraView;
     private static final int MY_CAMERA_REQUEST_CODE = 100;
 
@@ -82,15 +103,16 @@ public class MainActivity extends AppCompatActivity  implements /*OnTouchListene
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_main);
-
+        classNames = readLabels("labels.txt", this);
+        for(int i=0; i<classNames.size(); i++)
+            colors.add(randomColor());
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraPermissionGranted();
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
         }
-
-
 
     }
 
@@ -188,40 +210,96 @@ public class MainActivity extends AppCompatActivity  implements /*OnTouchListene
     /*public boolean onTouch(View v, MotionEvent event) {
         rett
     }*/
+    private List<String> readLabels (String file, Context context)
+    {
+        AssetManager assetManager = context.getAssets();
+        BufferedInputStream inputStream;
+        List<String> labelsArray = new ArrayList<>();
+        try {
+            // Read data from assets.
+            inputStream = new BufferedInputStream(assetManager.open(file));
+            byte[] data = new byte[inputStream.available()];
+            inputStream.read(data);
+            inputStream.close();
+            // Create copy file in storage.
+            File outFile = new File(context.getFilesDir(), file);
+            FileOutputStream os = new FileOutputStream(outFile);
+            os.write(data);
+            os.close();
+            Scanner fileScanner = new Scanner(new File(outFile.getAbsolutePath())).useDelimiter("\n");
+            String label;
+            while (fileScanner.hasNext()) {
+                label = fileScanner.next();
+                labelsArray.add(label);
+            }
+            fileScanner.close();
+        } catch (IOException ex) {
+            Log.i(TAG, "Failed to read labels!");
+        }
+        return labelsArray;
+    }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
         Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGBA2BGR);
-        /*Mat gray = new Mat(mRgba.height(),mRgba.width(),CvType.CV_8UC1);
-        Imgproc.cvtColor(mRgba,gray,Imgproc.COLOR_RGBA2GRAY);
+
+        /*Mat gray = new Mat(mRgba.height(),mRgba.width(),CvType.CV_8   UC1);
+        Imgproc.cvtColor(mRgba,gray,Imgproc.COLOR_RGBA2GRAY);Pixel_3a_API_30_x86
         Imgproc.threshold(gray,gray,10,255,Imgproc.THRESH_BINARY);  
         Imgproc.cvtColor(gray,mRgba,Imgproc.COLOR_GRAY2BGRA);*/
+
         Size frame_size = new Size(416, 416);
         Scalar mean = new Scalar(127.5);
         Mat blob = Dnn.blobFromImage(mRgba, 1.0 / 255.0, frame_size, mean, true, false);
+        List<Mat> result = new ArrayList<>();
+        List<String> outBlobNames = net.getUnconnectedOutLayersNames();
+
         if (net != null) {
             net.setInput(blob);
-            List<Mat> result = new ArrayList<>();
-            //List<String> outBlobNames = net.getUnconnectedOutLayers().
-            net.forward(result);
-            Log.i(TAG, "call");
+            net.forward(result,outBlobNames);
         }
-        else
-          Log.i(TAG, "null");
 
+        float confThreshold = 0.01f;
+        Log.i("WERKS", "#Detections"+result.size());
 
-        /*if (mIsColorSelected) {
-            mDetector.process(mRgba);
-            List<MatOfPoint> contours = mDetector.getContours();
-            Log.e(TAG, "Contours count: " + contours.size());
-            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+        //Log.i("WERKS","#labels"+classNames.size());
+        for (int i = 0;  i < result.size(); ++i) {
+            // each row is a candidate detection, the 1st 4 numbers are
+            // [center_x, center_y, width, height], followed by (N-4) class probabilities
+            Mat level = result.get(i);
+            for (int j = 0; j < level.rows(); ++j) {
+                Mat row = level.row(j);
+                Mat scores = row.colRange(5, level.cols());
+                Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
+                float confidence = (float) mm.maxVal;
+                Point classIdPoint = mm.maxLoc;
+                if (confidence > confThreshold) {
 
-            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
-            colorLabel.setTo(mBlobColorRgba);
+                    int centerX = (int) (row.get(0, 0)[0] * mRgba.cols());
+                    int centerY = (int) (row.get(0, 1)[0] * mRgba.rows());
+                    int width   = (int) (row.get(0,   2)[0] * mRgba.cols());
+                    int height  = (int) (row.get(0,  3)[0] * mRgba.rows());
 
-            Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
-            mSpectrum.copyTo(spectrumLabel);
-        }*/
+                    int left = (int) (centerX - width * 0.5);
+                    int top =(int)(centerY - height * 0.5);
+                    int right =(int)(centerX + width * 0.5);
+                    int bottom =(int)(centerY + height * 0.5);
+
+                    Point left_top = new Point(left, top);
+                    Point right_bottom=new Point(right, bottom);
+                    Point label_left_top = new Point(left, top-5);
+                    DecimalFormat df = new DecimalFormat("#.##");
+
+                    int class_id = (int) classIdPoint.x;
+                    String label= classNames.get(class_id) + ": " + df.format(confidence);
+                    Scalar color= colors.get(class_id);
+
+                    Imgproc.rectangle(mRgba, left_top,right_bottom , new Scalar(255,0,0), 3, 2);
+                    //Imgproc.putText(mRgba, label, label_left_top, 0, 1, new Scalar(0, 0, 0), 4);
+                    //Imgproc.putText(mRgba, label, label_left_top, 0, 1, new Scalar(255, 255, 255), 2);
+                }
+            }
+        }
 
         return mRgba;
     }
@@ -232,6 +310,29 @@ public class MainActivity extends AppCompatActivity  implements /*OnTouchListene
         Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
 
         return new Scalar(pointMatRgba.get(0, 0));
+    }
+
+    /*@Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(mOpenCvCameraView);
+    }*/
+
+    private Scalar randomColor() {
+        Random random = new Random();
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        return new Scalar(r,g,b);
+    }
+
+    private static List<String> getOutputNames(Net net) {
+        List<String> names = new ArrayList<>();
+
+        List<Integer> outLayers = net.getUnconnectedOutLayers().toList();
+        List<String> layersNames = net.getLayerNames();
+
+        outLayers.forEach((item) -> names.add(layersNames.get(item - 1)));
+        return names;
     }
 
 }
